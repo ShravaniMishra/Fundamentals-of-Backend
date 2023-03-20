05261fdf15b64333316cb3226e1bb885686f6ced
ShravaniMishra <shravniguddi@gmail.com>
2023-03-20 23:12:13 +0530

    bhow to use inbuild packages

---
+++ Basic Node/index.js
@@ -0,0 +1,9 @@
+// How to use inbuild node package
+
+const os = require("os")
+
+console.log(os)
+console.log(os.cpus())
+console.log(os.freemem())
+console.log(os.hostname())
+console.log(os.homedir())
