8a91a8af781a1ef283d82949cdc6099848aecea4
ShravaniMishra <shravniguddi@gmail.com>
2023-03-21 21:48:58 +0530

    Working with http-- nodemon---

--- Basic Node/index.js
+++ Basic Node/index.js
@@ -3,14 +3,17 @@
     // const os = require("os")
 
     // console.log(os)
+    // console.log(os.arch())
     // console.log(os.cpus())
     // console.log(os.freemem())
     // console.log(os.hostname())
     // console.log(os.homedir())
 
+
+
 // How to use file System(fs) inbuild node packages
 
-const fs = require('fs')
+// const fs = require('fs')
 
 // fs.readFile("Basic Node/something.txt",{encoding:"utf-8"},(err,data)=>{
 //     if(err){
@@ -50,18 +53,51 @@ const fs = require('fs')
 //     console.log(err)
 // }
 
-fs.appendFile("Basic Node/something.txt","\nThis is my third try\n",(err)=>{
-    if(err){
-        console.log(err)
-        console.log("Data is unable to be edited")
-    }else{
-        console.log("data is successfully updated")
+// fs.appendFile("Basic Node/something.txt","\nThis is my third try\n",(err)=>{
+//     if(err){
+//         console.log(err)
+//         console.log("Data is unable to be edited")
+//     }else{
+//         console.log("data is successfully updated")
+//     }
+// })
+
+// try{
+// fs.appendFileSync("Basic Node/something.txt","\nThis is my fourth try\n")
+// }catch(err){
+//     console.log("some issues has occured")
+//     console.log(err)
+// }
+
+// ****************************************
+
+// How to use HTTP in Node?
+
+const http = require("http")
+const fs = require("fs")
+    
+const server = http.createServer((req, res)=>{
+    if(req.url==="/"){
+        res.setHeader("Content-Type", "text/html")
+        res.end("<h1>Welcome To My Server!</h1>")
     }
+    else if(req.url==="/data"){
+        fs.readFile("./data.js",(err,data)=>{
+            if(err){
+                res.write(err)
+                res.end()
+            }else{
+                res.end(data)
+            }
+        })
+        
+    }else{
+        res.end("Invalid End Point 😭")
+    }
+
+})
+server.listen(2500,()=>{
+    console.log("Running at port - 3500")
 })
 
-try{
-fs.appendFileSync("Basic Node/something.txt","\nThis is my fourth try\n")
-}catch(err){
-    console.log("some issues has occured")
-    console.log(err)
-}
+
