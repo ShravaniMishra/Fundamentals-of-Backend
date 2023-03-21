d81c8ac81bd2b26d974f9b90ff8baed66fd68e73
ShravaniMishra <shravniguddi@gmail.com>
2023-03-21 19:48:13 +0530

    AppendFile & AppendFileSync

--- Basic Node/index.js
+++ Basic Node/index.js
@@ -34,18 +34,34 @@ const fs = require('fs')
 
 
 
-fs.writeFile("Basic Node/something.txt","\n This is my first try \n",(err)=>{
+// fs.writeFile("Basic Node/something.txt","\n This is my first try \n",(err)=>{
+//     if(err){
+//         console.log("Something went wrong")
+//         console.log(err)
+//     }else{
+//         console.log("Data is successfully written")
+//     }
+// })
+
+// try{
+//   fs.writeFileSync("Basic Node/something.txt","This is my second try")
+// }
+// catch(err){
+//     console.log(err)
+// }
+
+fs.appendFile("Basic Node/something.txt","\nThis is my third try\n",(err)=>{
     if(err){
-        console.log("Something went wrong")
         console.log(err)
+        console.log("Data is unable to be edited")
     }else{
-        console.log("Data is successfully written")
+        console.log("data is successfully updated")
     }
 })
 
 try{
-  fs.writeFileSync("Basic Node/something.txt","This is my second try")
-}
-catch(err){
+fs.appendFileSync("Basic Node/something.txt","\nThis is my fourth try\n")
+}catch(err){
+    console.log("some issues has occured")
     console.log(err)
 }
