99733905e8e1600100d8c0c1c80f4a6e2cb5bd6f
ShravaniMishra <shravniguddi@gmail.com>
2023-03-22 00:59:57 +0530

    method is working fine

--- Express/index.js
+++ Express/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/",(req,res)=>{
+    console.log(req.body)
     res.send("Welcome Bhai Log")
+})
+
+app.post("/addData",(req,res)=>{
+    console.log(req.body)
+    res.send("data has been added")
 })
 
 app.listen(8500,()=>{
